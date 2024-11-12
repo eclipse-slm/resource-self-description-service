b5bed9809121bb15b74398f24d0a9575ee4c5e7b
Ilgmi <michael.ilg@mailbox.org>
2024-11-12 12:46:40 +0100

    initial commit
    (cherry picked from commit a07b3c5e130f9730f961a9369dfb20b759ef71be)

---
+++ src/main/java/org/eclipse/slm/selfdescriptionservice/SelfDescriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package org.eclipse.slm.selfdescriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfDescriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfDescriptionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eclipse/slm/selfdescriptionservice/SelfDescriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eclipse.slm.selfdescriptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfDescriptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
