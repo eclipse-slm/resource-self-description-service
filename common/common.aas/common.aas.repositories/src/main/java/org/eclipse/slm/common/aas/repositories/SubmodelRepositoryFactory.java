4baf8b3606c68ecf8b471ea68803f67409085693
Ilgmi <michael.ilg@mailbox.org>
2024-11-26 17:28:05 +0100

    remove aas repository
    (cherry picked from commit 8e961f0fa24b7bdd38d7345b2e6b27efa6820467)

--- common/common.aas/common.aas.repositories/src/main/java/org/eclipse/slm/common/aas/repositories/SubmodelRepositoryFactory.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.slm.common.aas.repositories;
-
-import org.eclipse.digitaltwin.basyx.submodelrepository.SubmodelRepository;
-
-public interface SubmodelRepositoryFactory {
-
-    SubmodelRepository getSubmodelRepository(String aasId);
-
-}
