8cdb610bc29c8f29a04715307418d964935f56e6
Ilgmi <michael.ilg@mailbox.org>
2024-12-10 16:30:02 +0100

    change application.properties to application.yml
    (cherry picked from commit d9739f572060e9945b1e3ba31a33c8c2ca17f77c)

--- self_description_service.service/self_description_service.service.rest/src/main/java/org/eclipse/slm/self_description_service/service/rest/SelfDescriptionServiceApplication.java
+++ self_description_service.service/self_description_service.service.rest/src/main/java/org/eclipse/slm/self_description_service/service/rest/SelfDescriptionServiceApplication.java
@@ -2,8 +2,12 @@ package org.eclipse.slm.self_description_service.service.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableConfigurationProperties
 public class SelfDescriptionServiceApplication {
 
     public static void main(String[] args) {
