147b50351a8020106f59bcf4b9f3009f63fd3d8f
Ilgmi <michael.ilg@mailbox.org>
2024-12-10 17:25:14 +0100

    get resourceId from config create Submodel Id combined out of resourceId and Submodel ShortId
    (cherry picked from commit fd3451355a81255c6c4fad13d92b930dce2e1f07)

--- datasource/src/main/java/org/eclipse/slm/self_description_service/datasource/Template.java
+++ datasource/src/main/java/org/eclipse/slm/self_description_service/datasource/Template.java
@@ -3,10 +3,7 @@ package org.eclipse.slm.self_description_service.datasource;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.eclipse.digitaltwin.aas4j.v3.dataformat.aasx.AASXDeserializer;
 import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.DeserializationException;
-import org.eclipse.digitaltwin.aas4j.v3.model.Environment;
-import org.eclipse.digitaltwin.aas4j.v3.model.Property;
-import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
-import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
+import org.eclipse.digitaltwin.aas4j.v3.model.*;
 import org.eclipse.slm.self_description_service.datasource.template.ITemplateManager;
 import org.eclipse.slm.self_description_service.datasource.template.TemplateManager;
 import org.eclipse.slm.self_description_service.templating.TemplateRenderer;
@@ -14,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
@@ -27,18 +25,22 @@ import java.util.Optional;
 public class Template implements Datasource, InitializingBean {
     private final static Logger LOG = LoggerFactory.getLogger(Template.class);
 
+    private final String resourceId;
+
     private ITemplateManager templateManager;
     private TemplateRenderer renderer;
 
     private final HashMap<String, String> idToFileMap = new HashMap<>();
 
-    public Template(ITemplateManager templateManager) {
+    public Template(ITemplateManager templateManager, @Value("${resource.id}") String resourceId) {
+        this.resourceId = resourceId;
         this.templateManager = templateManager;
     }
 
     @Autowired
-    public Template(TemplateRenderer renderer) {
+    public Template(TemplateRenderer renderer, @Value("${resource.id}") String resourceId) {
         this.renderer = renderer;
+        this.resourceId = resourceId;
 
         templateManager = new TemplateManager();
     }
@@ -52,7 +54,8 @@ public class Template implements Datasource, InitializingBean {
             var environment = aasxDeserializer.read();
 
             for (Submodel submodel : environment.getSubmodels()) {
-                this.idToFileMap.put(submodel.getId(), template.getFilename());
+                var id = createSubmodelId(submodel.getIdShort());
+                this.idToFileMap.put(id, template.getFilename());
             }
 
         }
@@ -60,8 +63,6 @@ public class Template implements Datasource, InitializingBean {
 
 
     public List<Submodel> getModels() {
-
-        // TODO: Get ResourceID and use it in the ID to be global unique
 
         ArrayList<Submodel> submodels = new ArrayList<>();
 
@@ -76,6 +77,8 @@ public class Template implements Datasource, InitializingBean {
                 environment = aasxDeserializer.read();
 
                 for (Submodel submodel : environment.getSubmodels()) {
+                    var id = createSubmodelId(submodel.getIdShort());
+                    submodel.setId(id);
                     renderSubmodel(submodel);
                 }
                 submodels.addAll(environment.getSubmodels());
@@ -90,7 +93,7 @@ public class Template implements Datasource, InitializingBean {
 
     @Override
     public List<String> getModelIds() {
-        ArrayList<String> submodels = new ArrayList<>();
+        ArrayList<String> submodelIDs = new ArrayList<>();
 
         Resource[] templates;
         AASXDeserializer aasxDeserializer;
@@ -102,12 +105,11 @@ public class Template implements Datasource, InitializingBean {
                 var environment = aasxDeserializer.read();
 
                 for (Submodel submodel : environment.getSubmodels()) {
-                    submodels.add(submodel.getId());
+                    var id = createSubmodelId(submodel.getIdShort());
+                    submodelIDs.add(id);
 
-                    // TODO: combine resource id + template submodel short_id -> ID
-
-                    if (!idToFileMap.containsKey(submodel.getId())) {
-                        idToFileMap.put(submodel.getId(), template.getFilename());
+                    if (!idToFileMap.containsKey(id)) {
+                        idToFileMap.put(id, template.getFilename());
                     }
                 }
             }
@@ -116,34 +118,44 @@ public class Template implements Datasource, InitializingBean {
             throw new RuntimeException(e);
         }
 
-        return submodels;
+        return submodelIDs;
     }
 
     @Override
     public Optional<Submodel> getModelById(String id) {
         try {
+            Optional<Submodel> optionalSubmodel;
             if (!this.idToFileMap.containsKey(id)) {
-                return searchModelInTemplates(id);
+                optionalSubmodel = searchModelInTemplates(id);
+
+            } else {
+                var resource = this.templateManager.getTemplate(this.idToFileMap.get(id));
+
+                if (resource.isEmpty()) {
+                    return Optional.empty();
+                }
+
+                var aasxDeserializer = new AASXDeserializer(resource.get().getInputStream());
+                var environment = aasxDeserializer.read();
+
+                optionalSubmodel = environment.getSubmodels().stream().filter(model -> {
+                    var submodelId = createSubmodelId(model.getIdShort());
+                    return submodelId.equals(id);
+                }).findFirst();
             }
 
-            var resource = this.templateManager.getTemplate(this.idToFileMap.get(id));
 
-            if (resource.isEmpty()) {
-                return Optional.empty();
+            if (optionalSubmodel.isPresent()) {
+
+                var submodel = optionalSubmodel.get();
+                var submodelId = createSubmodelId(submodel.getIdShort());
+                submodel.setId(submodelId);
+
+                renderSubmodel(submodel);
+                return Optional.of(submodel);
             }
 
-
-            var aasxDeserializer = new AASXDeserializer(resource.get().getInputStream());
-            var environment = aasxDeserializer.read();
-
-            // TODO: combine resource id + template submodel short_id -> ID == id
-
-            var submodel = environment.getSubmodels().stream().filter(model -> model.getId().equals(id)).findFirst();
-            if (submodel.isPresent()) {
-                renderSubmodel(submodel.get());
-                return submodel;
-            }
-            return submodel;
+            return optionalSubmodel;
 
         } catch (InvalidFormatException | DeserializationException | IOException e) {
             LOG.error("Failed to get model by id with error message: {}", e.getMessage());
@@ -151,12 +163,26 @@ public class Template implements Datasource, InitializingBean {
         }
     }
 
-    private void renderSubmodel(Submodel m) {
-        if (m != null) {
-            for (SubmodelElement submodelElement : m.getSubmodelElements()) {
-                if (submodelElement instanceof Property property) {
-                    var value = this.renderer.render(property.getValue());
-                    property.setValue(value);
+    private void renderSubmodel(Submodel submodel) {
+        if (submodel != null) {
+            for (SubmodelElement element : submodel.getSubmodelElements()) {
+                renderSubmodelElement(element);
+            }
+        }
+    }
+
+    private void renderSubmodelElement(SubmodelElement element) {
+        if (element != null) {
+            if (element instanceof Property property) {
+                var value = this.renderer.render(property.getValue());
+                property.setValue(value);
+            } else if (element instanceof SubmodelElementList submodelElementList) {
+                for (SubmodelElement child : submodelElementList.getValue()) {
+                    renderSubmodelElement(child);
+                }
+            } else if (element instanceof SubmodelElementCollection submodelElementCollection) {
+                for (SubmodelElement child : submodelElementCollection.getValue()) {
+                    renderSubmodelElement(child);
                 }
             }
         }
@@ -173,7 +199,10 @@ public class Template implements Datasource, InitializingBean {
                 aasxDeserializer = new AASXDeserializer(template.getInputStream());
                 var environment = aasxDeserializer.read();
 
-                return environment.getSubmodels().stream().filter(submodel -> submodel.getId().equals(id)).findFirst();
+                return environment.getSubmodels().stream().filter(submodel -> {
+                    var submodelId = createSubmodelId(submodel.getIdShort());
+                    return submodelId.equals(id);
+                }).findFirst();
 
             }
         } catch (IOException | InvalidFormatException | DeserializationException e) {
@@ -184,6 +213,9 @@ public class Template implements Datasource, InitializingBean {
         return Optional.empty();
     }
 
+    private String createSubmodelId(String id) {
+        return this.resourceId + "_" + id;
+    }
 
     public void setTemplateManager(ITemplateManager templateManager) {
         this.templateManager = templateManager;
